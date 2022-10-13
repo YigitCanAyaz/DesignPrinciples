7ef35fd93a2c3dd25bcac16b8b25f03884587809
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-13 17:25:54 +0300

    Add project files.

---
+++ Loose_Coupling/Dependency.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Loose_Coupling_Dependency
+{
+    class MailSender
+    {
+        public void Send()
+        {
+            // Tight coupling
+            // If we create a new object inside of other object, it's coupling
+            //Gmail gmail = new Gmail();
+            //gmail.Send("ycanayaz2000@gmail.com");
+
+            Hotmail hotmail = new Hotmail();
+        }
+    }
+
+    class Gmail
+    {
+        public Gmail()
+        {
+
+        }
+
+        public void Send(string to)
+        {
+
+        }
+    }
+
+    class Hotmail
+    {
+
+    }
+}
---
+++ Loose_Coupling/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Loose_Coupling_InDependency.MailSender sender = new();
+sender.Send(new Loose_Coupling_InDependency.Gmail());
+sender.Send(new Loose_Coupling_InDependency.Hotmail());
+
+//-------
+
+// Bad version:
+
+Loose_Coupling_Dependency.MailSender sender2 = new();
+sender2.Send();
+sender2.Send();
