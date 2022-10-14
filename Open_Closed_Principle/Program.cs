cab7448e6ad1023ce0ead842ca26c334ce535d1c
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-14 03:24:16 +0300

    OpenClosedPrinciple commit

---
+++ Open_Closed_Principle/Program.cs
@@ -0,0 +1,7 @@
+﻿#region Not Ideal Code
+
+#endregion
+#region Ideal Code
+Open_Closed_Principle_IdealCode.ParaGonderici paraGonderici = new();
+paraGonderici.Gonder(new Open_Closed_Principle_IdealCode.AhmetBank(), 100, "asd");
+#endregion
